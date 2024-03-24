5c5baad809b58d9508c58413676acdb26a71025b
yunusonec <yunus81emre81.YEn@gmail.com>
2024-03-24 23:48:44 +0300

    Layout Bildirim Ayari 9.27

---
+++ Core_Proje/Areas/Writer/Controllers/DashboardController.cs
@@ -0,0 +1,43 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace Core_Proje.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class DashboardController : Controller
+    {
+        public readonly UserManager<WriterUser> _userManager;
+
+        public DashboardController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.v = values.Name + " " + values.SurName;
+
+
+            //Weather Api
+            string api = "cfec7cecb50ca6b8b3a514d91f37bcff";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=Duzce&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v5 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
+
+            //statistics
+            Context c = new Context();
+            ViewBag.v1 = 0;
+            ViewBag.v2 = c.Announcements.Count();
+            ViewBag.v3 = 0;
+            ViewBag.v4 = c.Skills.Count();
+            return View();
+        }
+    }
+}
+
+// https://api.openweathermap.org/data/2.5/weather?q=Düzce&appid=cfec7cecb50ca6b8b3a514d91f37bcff&units=metric&mode=xml
